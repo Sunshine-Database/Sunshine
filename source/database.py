9a5994f8ed28c636254b279b4eb7d2e55a26133f
De4oult <timofej.dimitrov@yandex.ru>
2023-04-27 00:03:41 +0300

    Database: push and get

--- source/database.py
+++ source/database.py
@@ -13,6 +13,7 @@ def create_database(filename: str, create_file: bool = True) -> True:
         if create_file and not os.path.exists(filename):
             with open(filename, 'w') as database_file:
                 database_file.write(json.dumps(EMPTY, indent = 4))
+    
 
 class Sunshine:
     def __init__(self, filename: str, id_field: str = 'id') -> None:
@@ -25,6 +26,9 @@ class Sunshine:
     def _get_id(self) -> int:
         return int(str(uuid.uuid4().int)[:18])
 
+    def _get_load_function(self):
+        return json.load
+
     def _get_dump_function(self):
         return json.dump
 
@@ -34,22 +38,66 @@ class Sunshine:
                 database_data = json.load(database_file)
                 try:
                     if set(database_data['data'][0].keys()) == set(data_to_push.keys()).union([self._id_field]):
-                        data_to_push[self._id_field] = self._get_id()
+                        data_to_push = {self._id_field : self._get_id()} | data_to_push 
 
                         database_data['data'].append(data_to_push)
                         database_file.seek(0)
                         self._get_dump_function()(database_data, database_file, indent = 4, ensure_ascii = False)
                         
-                        return data_to_push[self._id_field] 
+                        return data_to_push[self._id_field]
                     
                     else:
-                        print("Model Error") # later replace with raise ModelError
-                        exit()
+                        data_to_push = {self._id_field : self._get_id()} | data_to_push
+
+                        print(database_data['data'])
+
+                        for i in range(len(database_data['data'])):
+                            for key in set(data_to_push.keys()).union([self._id_field]) - set(database_data['data'][0].keys()):
+                                database_data['data'][i][str(key)] = ''
+
+                        database_data['data'].append(data_to_push)
+                        database_file.seek(0)
+                        self._get_dump_function()(database_data, database_file, indent = 4, ensure_ascii = False)
+                        
+                        return data_to_push[self._id_field]
 
                 except IndexError:
-                    data_to_push[self._id_field] = self._get_id()
+                    data_to_push = {self._id_field : self._get_id()} | data_to_push 
                     database_data['data'].append(data_to_push)
                     database_file.seek(0)
                     self._get_dump_function()(database_data, database_file, indent = 4, ensure_ascii = False)
 
                     return data_to_push[self._id_field]
+                
+    def take(self, query: dict = '', count: int = 1) -> list[dict[str, any]]:
+        with self.lock:
+            if not query:
+                try:
+                    with open(self.filename, 'r', encoding = 'utf-8') as database_file:
+                        database_data = self._get_load_function()(database_file)
+                    
+                    if count <= len(database_data['data']):
+                        data = database_data['data'][0: int(count)]
+                        return data
+                    
+                    else:
+                        print('Error: out of range') # raise LengthError
+                
+                except:
+                    return [{'' : ''}]
+            
+            elif query and count == 1:
+                result = []
+                with open(self.filename, 'r', encoding = 'utf-8') as database_file:
+                    database_data = self._get_load_function()(database_file)
+                    for data in database_data['data']:
+                        if all(x in data and data[x] == query[x] for x in query):
+                            result.append(data)
+
+                return result
+
+            else:
+                print('Error: do not use query and count queries in one callback')
+                
+                return [{'' : ''}]
+            
--- source/test.py
+++ source/test.py
@@ -4,17 +4,7 @@ from database import Sunshine
 db = Sunshine('./database/test.json')
 print(db)
 
-db.push(
-    {
-        "name"   : "Andrew",
-        "gender" : "male",
-        "job"    : "solo developer"
-    },
-)
-db.push(
-    {
-        "name"   : "Susan",
-        "gender" : "female",
-        "job"    : "fullstack-developer"
-    }
-)
+print(db.take(count = 1))
+
+print(db.take({'name' : 'Susan'}))
+print(db.take({'name' : 'Olga'}))
