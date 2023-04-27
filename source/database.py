aa4333e6252018b4aecf90688080ec4e23346d7e
De4oult <timofej.dimitrov@yandex.ru>
2023-04-27 11:15:00 +0300

    Database: delete, drop

--- source/database.py
+++ source/database.py
@@ -26,6 +26,9 @@ class Sunshine:
     def _get_id(self) -> int:
         return int(str(uuid.uuid4().int)[:18])
 
+    def _cast_id(self, id: int) -> int:
+        return int(id)
+
     def _get_load_function(self):
         return json.load
 
@@ -35,7 +38,7 @@ class Sunshine:
     def push(self, data_to_push: dict[str, any]) -> int:
         with self.lock:
             with open(self.filename, 'r+', encoding = 'utf-8') as database_file:
-                database_data = json.load(database_file)
+                database_data: dict = json.load(database_file)
                 try:
                     if set(database_data['data'][0].keys()) == set(data_to_push.keys()).union([self._id_field]):
                         data_to_push = {self._id_field : self._get_id()} | data_to_push 
@@ -48,8 +51,6 @@ class Sunshine:
                     
                     else:
                         data_to_push = {self._id_field : self._get_id()} | data_to_push
-
-                        print(database_data['data'])
 
                         for i in range(len(database_data['data'])):
                             for key in set(data_to_push.keys()).union([self._id_field]) - set(database_data['data'][0].keys()):
@@ -69,7 +70,7 @@ class Sunshine:
 
                     return data_to_push[self._id_field]
                 
-    def take(self, query: dict = '', count: int = 1) -> list[dict[str, any]]:
+    def get(self, query: dict = {}, count: int = 1) -> list[dict[str, any]]:
         with self.lock:
             if not query:
                 try:
@@ -100,4 +101,43 @@ class Sunshine:
                 print('Error: do not use query and count queries in one callback')
                 
                 return [{'' : ''}]
-            
+        
+    # def update(self, data_to_update: dict[str, any]) -> None:
+    #     updated: bool = False
+        
+    #     with self.lock:
+    #         with open(self.filename, 'r+', encoding = 'utf-8') as database_file:
+    #             database_data: dict = self._get_load_function()(database_file)
+    #             result: list = []
+
+    #             if set(data_to_update.keys()).issubset(database_data['data'][0].keys()):
+    #                 for data in database_data['data']:
+    #                     if data[self._id_field] == self
+
+    def delete(self, id: int = 0) -> bool:
+        with self.lock:
+            with open(self.filename, 'r+', encoding = 'utf-8') as database_file:
+                database_data: dict = self._get_load_function()(database_file)
+                result: list = []
+                found: bool  = False
+
+                for data in database_data['data']:
+                    if data[self._id_field] == self._cast_id(id):
+                        found = True
+                    else:
+                        result.append(data)
+
+                if not found:
+                    pass # raise IdNotFoundError
+
+                database_data['data'] = result
+                database_file.seek(0)
+                database_file.truncate()
+                self._get_dump_function()(database_data, database_file, indent = 4, ensure_ascii = False)
+
+            return True
+        
+    def drop(self) -> None:
+        with self.lock:
+            with open(self.filename, 'w', encoding = 'utf-8') as database_file:
+                database_file.write(json.dumps(EMPTY, indent = 4))
--- source/test.py
+++ source/test.py
@@ -1,10 +1,25 @@
 from database import Sunshine
 
+import time
 
 db = Sunshine('./database/test.json')
 print(db)
 
-print(db.take(count = 1))
+print(db.push({
+    'name'   : 'Susan',
+    'gender' : 'Female',
+    'age'    : 22,
+    'job'    : 'React-developer'
+}))
 
-print(db.take({'name' : 'Susan'}))
-print(db.take({'name' : 'Olga'}))
+print(db.push({
+    'name'   : 'Mark',
+    'gender' : 'Male',
+    'age'    : 25,
+}))
+
+print(db.get({'name' : 'Susan'}))
+time.sleep(3)
+print(db.delete(db.get({'name' : 'Mark'})[0]['id']))
+time.sleep(3)
+print(db.drop())
