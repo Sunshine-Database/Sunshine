e9ccffbaa3fcc1998a6c195ebcf817fcbc1a0f39
De4oult <timofej.dimitrov@yandex.ru>
2023-05-31 14:13:30 +0300

    Documentation for an methods

--- source/sunshine.py
+++ source/sunshine.py
@@ -1,6 +1,7 @@
 from itertools import groupby
-from filelock import FileLock
-from datetime import datetime
+from filelock  import FileLock
+from datetime  import datetime
+from messages  import Message
 
 import shutil
 import uuid
@@ -19,6 +20,10 @@ def create_database(filename: str, create_file: bool = True) -> True:
     
 
 class Sunshine:
+    """
+    Class of json-database. 
+    Has the following set of methods: push, all, get, update, contains, delete, drop, backup.
+    """
     def __init__(self, filename: str, id_field: str = 'id') -> None:
         create_database(filename)
         
@@ -39,6 +44,11 @@ class Sunshine:
         return json.dump
     
     def push(self, data_to_push: dict[str, any]):
+        """
+        Adds an object with the given fields to the database.
+        Requires one argument:
+            data_to_push (dictionary[string, any]) - the key-value dictionary to be added to the database.
+        """
         with self.lock:
             with open(self.filename, 'r+', encoding = 'utf-8') as database_file:
                 database_data: dict = json.load(database_file)
@@ -51,12 +61,23 @@ class Sunshine:
                         
                 return data_to_push[self._id_field]
 
-    def get_all(self) -> list[dict[str, any]]:
+    def all(self) -> list[dict[str, any]]:
+        """
+        Returns all database objects.
+        """
         with self.lock:
             with open(self.filename, 'r', encoding = 'utf-8') as database_file:
                 return self._get_load_function()(database_file)['data']
 
     def get(self, query: dict[str, any] = {}, count: int = 1) -> list[dict[str, any]]:
+        """
+        Returns a database object using a query, or returns the number of given elements in the first one up to the number specified in the `count` argument.
+        Requires two arguments:
+            query (dictionary[string, any]) - a key-value dictionary that will be used to select elements,
+            count (integer)                 - the number of requested elements.
+
+        You cannot use both arguments together.
+        """
         with self.lock:
             with open(self.filename, 'r', encoding = 'utf-8') as database_file:
                 database_data = self._get_load_function()(database_file)
@@ -66,7 +87,7 @@ class Sunshine:
                         return data
                     
                     else:
-                        print('Out of range')
+                        Message('index out of range', 'error')
 
                 elif query and count == 1:
                     result: dict = []
@@ -79,9 +100,15 @@ class Sunshine:
                     return result[0] if len(result) == 1 else result
 
                 else:
-                    raise Exception('Error: do not use query and count queries in one request')
+                    Message('do not use query and counter in one get-query', 'error')
 
     def update(self, id: int, data_to_update: dict[str, any]) -> int:
+        """
+        Updates a database object with an ID.
+        Requires two arguments:
+            id             (14-digit integer)        - numeric identifier,
+            data_to_update (dictionary[string, any]) - the key-value dictionary that will be updated in the database object.
+        """
         with self.lock:
             with open(self.filename, 'r+', encoding = 'utf-8') as database_file:
                 database_data = self._get_load_function()(database_file)
@@ -96,7 +123,7 @@ class Sunshine:
                     result.append(data)
                 
                 if not updated:
-                    raise Exception('Error: there is no element with given `id`')
+                    Message('there is no element with given `id`', 'error')
                 
                 database_data["data"] = result
                 database_file.seek(0)
@@ -106,9 +133,21 @@ class Sunshine:
         return 0
 
     def contains(self, key: str, value: any) -> bool:
+        """
+        Checks by query, if an element is contained in the database.
+        Requires two arguments:
+            key   (string),
+            value (any).
+        These arguments will be searched in the database.
+        """
         return True if self.get(query = {key : value}) != [] else False
 
     def delete(self, id: int) -> int:
+        """
+        Removes the object with the given ID from the database.
+        Requires one argument:
+            id (14-digit integer) - numeric identifier,
+        """
         with self.lock:
             with open(self.filename, 'r+', encoding = 'utf-8') as database_file:
                 database_data = self._get_load_function()(database_file)
@@ -122,7 +161,7 @@ class Sunshine:
                         result.append(data)
 
                 if not found:
-                    raise Exception('Error: there is no element with given `id`')
+                    Message('there is no element with given `id`', 'error')
 
                 database_data['data'] = result
                 database_file.seek(0)
@@ -130,14 +169,22 @@ class Sunshine:
                 self._get_dump_function()(database_data, database_file, indent = 4, ensure_ascii = False)
 
         return 0
+    
+    def drop(self) -> None:
+        """
+        Removes all objects from the database.
+        """
+        with self.lock:
+            with open(self.filename, 'w', encoding = 'utf-8') as database_file:
+                database_file.write(json.dumps(EMPTY, indent = 4))
 
     def backup(self, path: str) -> int:
+        """
+        Creates a database backup at the given path.
+        Requires one argument:
+            path (string) - path to the folder where the backup-file will be saved.
+        """
         if not os.path.exists(path): os.mkdir(path)
         if '/' in self.filename: filename = self.filename.split('/')[-1]
         shutil.copy(self.filename, f'{path}/{datetime.strftime(datetime.now(), "%H:%M:%S.%f-%d-%m-%Y")}-{filename}')
         return 0
-    
-    def drop(self) -> None:
-        with self.lock:
-            with open(self.filename, 'w', encoding = 'utf-8') as database_file:
-                database_file.write(json.dumps(EMPTY, indent = 4))
